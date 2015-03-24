0fa12d73c8e9011a30ecc79a62e71f3d8c59cb68
Nick Markkula <nickmkk@gmail.com>
2015-03-24 14:04:44 -0500

    added the project

---
+++ O365WebClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(O365WebClient.Startup))]
+namespace O365WebClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

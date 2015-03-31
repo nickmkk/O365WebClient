e7d8db03b98045ccfd27ee51a3fc45ac338a18a5
Nick <nickmkk@gmail.com>
2015-03-31 16:55:52 -0500

    finally figured out how to parse the access token dates

--- O365WebClient/Scripts/AuthTokenGenerator.js
+++ O365WebClient/Scripts/AuthTokenGenerator.js
@@ -1,27 +1,27 @@
 var AuthTokenGenerator = (function () {
     function AuthTokenGenerator(code) {
-        this.code = code;
-    }
-    AuthTokenGenerator.prototype.generateToken = function (tokenCallback) {
-        var _this = this;
+        this.authCode = code;
         //todo: figure out how to get the following from config
-        var apiRootUrl = "http://localhost:23086";
-        var clientId = "c8779caa-78e9-4039-ad69-ebb1a66324e1";
-        var redirectUri = location.origin + "/account/mailboxlogin";
-        var appSecret = "tPQf1p7TBHHXgl3EjRkIZduY85DleuBYT45qUDtdJsk=";
-        var resource = "https://outlook.office365.com";
+        this.apiRootUrl = "http://localhost:23086";
+        this.clientId = "c8779caa-78e9-4039-ad69-ebb1a66324e1";
+        this.redirectUri = location.origin + "/account/mailboxlogin";
+        this.appSecret = "tPQf1p7TBHHXgl3EjRkIZduY85DleuBYT45qUDtdJsk=";
+        this.resource = "https://outlook.office365.com";
+    }
+    AuthTokenGenerator.prototype.generateToken = function (grantType, code, tokenCallback) {
+        var _this = this;
         jQuery.ajax({
             type: 'Post',
-            url: apiRootUrl + "/common/oauth2/token",
+            url: this.apiRootUrl + "/common/oauth2/token",
             contentType: "application/x-www-form-urlencoded",
-            data: "client_id=" + clientId + "&redirect_uri=" + redirectUri + "&client_secret=" + appSecret + "&grant_type=authorization_code&code=" + this.code + "&resource=" + resource,
+            data: "client_id=" + this.clientId + "&redirect_uri=" + this.redirectUri + "" + "&client_secret=" + this.appSecret + "" + "&grant_type=" + grantType + "&code=" + code + "&resource=" + this.resource,
             xhrFields: {
                 withCredentials: false
             },
             headers: {},
-            success: function (token, textStatus, jqXhr) {
-                _this.token = token;
-                tokenCallback(_this.token.token_type + " " + _this.token.access_token);
+            success: function (accessToken, textStatus, jqXhr) {
+                _this.accessToken = accessToken;
+                tokenCallback(_this.accessToken.token_type + " " + _this.accessToken.access_token);
             },
             error: function (jqXhr, textStatus, errorThrown) {
                 debugger;
@@ -29,12 +29,19 @@ var AuthTokenGenerator = (function () {
         });
     };
     AuthTokenGenerator.prototype.getAccessToken = function (tokenCallback) {
-        //todo: if the token is expired then get a new one
-        if (typeof (this.token) != "undefined") {
-            tokenCallback(this.token.token_type + " " + this.token.access_token);
+        if (typeof (this.accessToken) == "undefined") {
+            this.generateToken("authorization_code", this.authCode, tokenCallback);
             return;
         }
-        this.generateToken(tokenCallback);
+        var tokenExpires = new Date(this.accessToken.expires_on * 1000);
+        var expired = new Date() > tokenExpires;
+        debugger;
+        if (expired) {
+            this.generateToken("refresh_token", this.accessToken.refresh_token, tokenCallback);
+        }
+        else {
+            tokenCallback(this.accessToken.token_type + " " + this.accessToken.access_token);
+        }
     };
     return AuthTokenGenerator;
 })();
--- O365WebClient/Scripts/AuthTokenGenerator.ts
+++ O365WebClient/Scripts/AuthTokenGenerator.ts
@@ -1,33 +1,44 @@
 ﻿class AuthTokenGenerator {
-    private code: string;
-    private token: AccessToken;
+    private authCode: string;
+    private accessToken: AccessToken;
+
+    //config
+    private apiRootUrl: string;
+    private clientId: string;
+    private redirectUri: string;
+    private appSecret: string;
+    private resource: string;
 
     constructor(code: string) {
-        this.code = code;
+        this.authCode = code;
+        //todo: figure out how to get the following from config
+        this.apiRootUrl = "http://localhost:23086";
+        this.clientId = "c8779caa-78e9-4039-ad69-ebb1a66324e1";
+        this.redirectUri = (<any>location).origin + "/account/mailboxlogin";
+        this.appSecret = "tPQf1p7TBHHXgl3EjRkIZduY85DleuBYT45qUDtdJsk=";
+        this.resource = "https://outlook.office365.com";
     }
 
-    private generateToken(tokenCallback: (token: string) => void) {
-        //todo: figure out how to get the following from config
-        var apiRootUrl = "http://localhost:23086";
-        var clientId = "c8779caa-78e9-4039-ad69-ebb1a66324e1";
-        var redirectUri = (<any>location).origin + "/account/mailboxlogin";
-        var appSecret = "tPQf1p7TBHHXgl3EjRkIZduY85DleuBYT45qUDtdJsk=";
-        var resource = "https://outlook.office365.com";
-
+    private generateToken(grantType: string, code: string, tokenCallback: (token: string) => void) {
         jQuery.ajax({
             type: 'Post',
-            url: apiRootUrl + "/common/oauth2/token",
+            url: this.apiRootUrl + "/common/oauth2/token",
             contentType: "application/x-www-form-urlencoded",
-            data: "client_id=" + clientId + "&redirect_uri=" + redirectUri + "&client_secret=" + appSecret + "&grant_type=authorization_code&code=" + this.code + "&resource=" + resource,
+            data: "client_id=" + this.clientId +
+            "&redirect_uri=" + this.redirectUri + "" +
+            "&client_secret=" + this.appSecret + "" +
+            "&grant_type=" + grantType +
+            "&code=" + code +
+            "&resource=" + this.resource,
 
             xhrFields: {
                 withCredentials: false
             },
             headers: {},
 
-            success: (token: AccessToken, textStatus: string, jqXhr: JQueryXHR) => {
-                this.token = token;
-                tokenCallback(this.token.token_type + " " + this.token.access_token);
+            success: (accessToken: AccessToken, textStatus: string, jqXhr: JQueryXHR) => {
+                this.accessToken = accessToken;
+                tokenCallback(this.accessToken.token_type + " " + this.accessToken.access_token);
             },
             error: (jqXhr: JQueryXHR, textStatus: string, errorThrown: string) => {
                 debugger;
@@ -36,25 +47,32 @@
     }
 
     public getAccessToken(tokenCallback: (token: string) => void) {
-        //todo: if the token is expired then get a new one
-        if (typeof (this.token) != "undefined") {
-            tokenCallback(this.token.token_type + " " + this.token.access_token);
+        if (typeof (this.accessToken) == "undefined") {
+            this.generateToken("authorization_code", this.authCode, tokenCallback);
             return;
         }
-        this.generateToken(tokenCallback);
+        var tokenExpires = new Date(this.accessToken.expires_on * 1000);
+        var expired = new Date() > tokenExpires;
+        debugger;
+        if (expired) {
+            this.generateToken("refresh_token", this.accessToken.refresh_token, tokenCallback);
+        } else {
+            tokenCallback(this.accessToken.token_type + " " + this.accessToken.access_token);
+        }
     }
+
 }
 
 class AccessToken {
     token_type: string;
-    expires_in: string;
-    expires_on: string;
-    not_before: string;
+    expires_in: number;
+    expires_on: number;
+    not_before: number;
     resource: string;
     access_token: string;
     refresh_token: string;
     scope: string;
     id_token: string;
-    pwd_exp: string;
+    pwd_exp: number;
     pwd_url: string;
 }

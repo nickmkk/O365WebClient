a8989b0b561763bf1832a07cf6b6ba35f7c2d572
Nick <nickmkk@gmail.com>
2015-04-01 13:37:53 -0500

    moved the mailbox content dto's out of the mail controller

--- O365WebApi/Controllers/MailController.cs
+++ O365WebApi/Controllers/MailController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -27,59 +26,56 @@ namespace O365WebApi.Controllers
             return content;
         }
 
-        #region "Response Content" 
-        
-        public class Body
-        {
-            public string ContentType { get; set; }
-            public string Content { get; set; }
-        }
-
-        public class EmailAddress
-        {
-            public string Address { get; set; }
-            public string Name { get; set; }
-        }
-
-        public class Recipient
-        {
-            public EmailAddress EmailAddress { get; set; }
-        }
-
-        public class MailItem
-        {
-            public string Id { get; set; }
-            public string ChangeKey { get; set; }
-            public List<object> Categories { get; set; }
-            public string DateTimeCreated { get; set; }
-            public string DateTimeLastModified { get; set; }
-            public string Subject { get; set; }
-            public string BodyPreview { get; set; }
-            public Body Body { get; set; }
-            public string Importance { get; set; }
-            public bool HasAttachments { get; set; }
-            public string ParentFolderId { get; set; }
-            public Recipient From { get; set; }
-            public Recipient Sender { get; set; }
-            public List<Recipient> ToRecipients { get; set; }
-            public List<object> CcRecipients { get; set; }
-            public List<object> BccRecipients { get; set; }
-            public List<object> ReplyTo { get; set; }
-            public string ConversationId { get; set; }
-            public string DateTimeReceived { get; set; }
-            public string DateTimeSent { get; set; }
-            public bool? IsDeliveryReceiptRequested { get; set; }
-            public bool IsReadReceiptRequested { get; set; }
-            public bool IsDraft { get; set; }
-            public bool IsRead { get; set; }
-        }
-
-        public class MailboxContent
-        {
-            public List<MailItem> value { get; set; }
-        }
-
-        #endregion
-
     }
+
+    public class MailboxContent
+    {
+        public List<MailItem> value { get; set; }
+    }
+
+    public class MailItem
+    {
+        public string Id { get; set; }
+        public string ChangeKey { get; set; }
+        public List<object> Categories { get; set; }
+        public string DateTimeCreated { get; set; }
+        public string DateTimeLastModified { get; set; }
+        public string Subject { get; set; }
+        public string BodyPreview { get; set; }
+        public Body Body { get; set; }
+        public string Importance { get; set; }
+        public bool HasAttachments { get; set; }
+        public string ParentFolderId { get; set; }
+        public Recipient From { get; set; }
+        public Recipient Sender { get; set; }
+        public List<Recipient> ToRecipients { get; set; }
+        public List<object> CcRecipients { get; set; }
+        public List<object> BccRecipients { get; set; }
+        public List<object> ReplyTo { get; set; }
+        public string ConversationId { get; set; }
+        public string DateTimeReceived { get; set; }
+        public string DateTimeSent { get; set; }
+        public bool? IsDeliveryReceiptRequested { get; set; }
+        public bool IsReadReceiptRequested { get; set; }
+        public bool IsDraft { get; set; }
+        public bool IsRead { get; set; }
+    }
+
+    public class Body
+    {
+        public string ContentType { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class EmailAddress
+    {
+        public string Address { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Recipient
+    {
+        public EmailAddress EmailAddress { get; set; }
+    }
+
 }
